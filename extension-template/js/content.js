ce8a3e0befe431ff1a4d47502d0893f329d23fd1
Josh Dixon <jdixon675@gmail.com>
2021-02-23 14:51:07 -0700

    event listener attempt

--- extension-template/js/content.js
+++ extension-template/js/content.js
@@ -2,19 +2,37 @@ console.log("Extension is running!");
 
 // basic replace function taken from https://github.com/MysteryPancake/Image-Replacer/blob/master/scripts/Main.js
 // modified to work with pixelate.js function
-
-/* Proof of concept:
-replace ();
+replace();
 
 function replace() {
-    console.log("entered function");
-    window.setInterval(replace, 2000);
-	var images = document.getElementsByTagName("img");
-	for (var i = 0; i < images.length; i++) {
-        $(images[i]).pixelate();       
-    }
+	console.log("entered function");
+	var images = document.querySelectorAll("img");
+
+	function addImages () {
+		console.log('adding event listener')
+		addImages.addEventListener('load', function () {
+			console.log('event listener activated')
+			images.push(document.querySelectorAll('img'));
+			replace();
+		});
+	}
+
+	addImages();
+	pixels(images);
 }
-*/
+
+// function addImages () {
+// 	addImages.addEventListener('load',function () {
+// 		document.querySelectorAll('img').appendChild(images);
+// 		replace();
+// 	});
+// }
+
+function pixels (images) {
+	for (var i = 0; i < images.length; i++) {
+		$(images[i]).pixelate();       
+	}
+}
 
 
 // Attempt with MutationObeserver
@@ -30,23 +48,22 @@ function replace() {
 // 	subtree: false
 // });
 
+// var imgOnPage = [];
 
-var imgOnPage = [];
+// function addImages () {
+// 	imgOnPage = document.getElementsByTagName('img');
+// 	pixelateImgs();
+// };
 
-function addImages () {
-	document.getElementsByTagName('img');
-	pixelateImgs();
-};
+// // addImages.addEventListener('load', function () {
+// // 	document.getElementsByTagName('img').appendChild(imgOnPage);
+// // });
 
-addImages.addEventListener('load', function () {
-	document.getElementsByTagName('img').appendChild(imgOnPage);
-});
-
-function pixelateImgs () {
-	for (var x = 0; x < imgOnPage.length; x++) {
-		imgOnPage[x].pixelate.js();
-	}
-}
+// function pixelateImgs () {
+// 	for (var x = 0; x < imgOnPage.length; x++) {
+// 		imgOnPage[x].pixelate();
+// 	}
+// }
 
 
 
